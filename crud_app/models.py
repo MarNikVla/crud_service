d0bddf1fc67edf40391bc2ececb94323833da237
marnikvla <condor52@yandex.ru>
2021-07-24 18:17:48 +0300

    test

--- crud_app/models.py
+++ crud_app/models.py
@@ -8,10 +8,14 @@ class CompanyModel(models.Model):
 
 
 class UserModel(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
     name = models.CharField(max_length=80)
-    sex = models.CharField(max_length=15)
-    age = models.IntegerField()
-    group = models.ForeignKey(to='CompanyModel', on_delete=models.CASCADE)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    age = models.IntegerField(max_length=3)
+    company = models.ForeignKey(CompanyModel, on_delete=models.CASCADE, related_name='users')
 
     def __str__(self):
         return self.name
--- crud_service/settings.py
+++ crud_service/settings.py
@@ -13,14 +13,17 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+#setup env from .env
+dotenv_file = Path(BASE_DIR, ".env")
+if Path.is_file(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k*9(zn-4ec##dx^otq02_+(rvufj%h*bua-gv@&1k_h5hignp%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
