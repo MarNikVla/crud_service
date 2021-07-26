42e0a0b3c17885f3ed1b8ca160184fbad11079d5
marnikvla <condor52@yandex.ru>
2021-07-26 16:02:47 +0300

    testing 2

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 
 from crud_app import views
 from crud_app.views import IndexView
+from django.contrib.auth import views as auth_views
 
 app_name = 'shop'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    # path('<slug:category_slug>/', views.product_list,
-    # name='product_list_by_category'),
-    # path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('', IndexView.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('registration/', views.RegisterUserFormView.as_view(), name='registration'),
+    path('update/update_done/', views.ProfileUpdateDoneView.as_view(), name='update_done'),
+    path('update/', views.ProfileUpdateView.as_view(), name='update_profile'),
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from crud_app.models import UserModel,CompanyModel
+
 
 class IndexView(TemplateView):
     template_name = "base.html"
@@ -10,3 +11,25 @@ class IndexView(TemplateView):
         context['user'] = UserModel.objects.all()
         context['company'] = CompanyModel.objects.all()
         return context
+
+
+class CompanyDetailView(DetailView):
+    model = CompanyModel
+    template_name = 'crud_app/company_detail.html'
+    context_object_name = 'company'
+
+    def get_queryset(self):
+        company = self.kwargs.get('company_title', '')
+        q = super().get_queryset()
+        return q.filter(company__name=company)
+
+
+def RegisterUserFormView(FormView):
+    """Standard registration view"""
+    form_class = UserCreationForm
+    template_name = "registration/register.html"
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterUserFormView, self).form_valid(form)
